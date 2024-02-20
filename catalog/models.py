b9aab9d14db4620507f487a3e52e815fc179a83d
petr-fol <144835428+petr-fol@users.noreply.github.com>
2024-02-20 17:00:55 +0600

    создание модели категорий

--- catalog/models.py
+++ catalog/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     description = models.TextField(verbose_name='Описание')
     price = models.IntegerField(default=0, verbose_name='Цена')
     image = models.ImageField(upload_to='images', null=True, blank=True, verbose_name='Изображение', default='images/default.jpg')
+    category = models.ForeignKey('catalog.Category', on_delete=models.CASCADE, verbose_name='Категория', default='Продукты')
 
     class Meta:
         verbose_name = 'Товар'
@@ -13,3 +14,17 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.price})"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
