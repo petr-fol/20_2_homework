0070125c476abc0e47822176e786ab0c1453e6d8
Petr <folmerpetr21@gmail.com>
2024-04-29 02:01:49 +0500

    починил слаги и возобновил работу, далее точечные коммиты

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from catalog.views import ProductDetailView, ProductUpdateView, ProductDeleteView, ProductCreateView, ProductListView
+from catalog.views import (ProductDetailView,
+                           ProductUpdateView, ProductDeleteView, ProductCreateView, ProductListView)
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('edit/<int:pk>/', ProductUpdateView.as_view(), name='product_form'),
-    path('confirm_delete/<int:pk>/', ProductDeleteView.as_view(), name='product_confirm_delete'),
+    path('detail/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('edit/<slug:slug>/', ProductUpdateView.as_view(), name='product_form'),
+    path('confirm_delete/<pk>/', ProductDeleteView.as_view(), name='product_confirm_delete'),
     path('create/', ProductCreateView.as_view(), name='product_create'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- config/settings.py
+++ config/settings.py
@@ -65,10 +65,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_1',
+        'USER': 'postgres',
+        'PASSWORD': '0001',
+        'HOST': 'localhost',  # 'localhost' или '127.0.0.1'
+        'PORT': '5432',  # По умолчанию 5432
     }
 }
+
 
 
 # Password validation
