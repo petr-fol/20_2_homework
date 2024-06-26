27f43a11c6ce426ab2e6195d3f41188b0614a78b
Petr <folmerpetr21@gmail.com>
2024-06-27 01:57:58 +0500

    удаление лишнего кода от формсета

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,13 @@ from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
-from blog.forms import MessageForm, SubjectForm
-from blog.models import Message, Subject
+from blog.forms import MessageForm
+from blog.models import Message
 
 
 class MessageListView(ListView):
     model = Message
-    template_name = 'message_list.html'
+    # template_name = 'message_list.html'
     context_object_name = 'messages'
 
     def get_queryset(self):
@@ -41,27 +41,17 @@ class MessageUpdateView(UpdateView):
     model = Message
     form_class = MessageForm
 
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        SubjectFormset = inlineformset_factory(Message, Subject, form=SubjectForm, extra=1)
-        if self.request.method == 'POST':
-            context_data['formset'] = SubjectFormset(self.request.POST, instance=self.object)
-        else:
-            context_data['formset'] = SubjectFormset(instance=self.object)
-
-        return context_data
+    # def get_context_data(self, **kwargs):
+    #     context_data = super().get_context_data(**kwargs)
+    #     if self.request.method == 'POST':
+    #         context_data['formset'] = SubjectFormset(self.request.POST, instance=self.object)
+    #     else:
+    #         context_data['formset'] = SubjectFormset(instance=self.object)
+    #
+    #     return context_data
 
     def get_success_url(self):
         return reverse_lazy('message_detail', kwargs={'slug': self.object.slug})
-
-    def form_valid(self, form):
-        formset = self.get_context_data()['formset']
-        self.object = form.save()
-        if formset.is_valid():
-            formset.instance = self.object
-            formset.save()
-
-        return super().form_valid(form)
 
 
 class MessageDeleteView(DeleteView):
