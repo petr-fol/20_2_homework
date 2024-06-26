9bf9517908ba33163dfb9f1e292b2a116414aaea
Petr <folmerpetr21@gmail.com>
2024-06-27 01:57:38 +0500

    настройка урлов

--- students/urls.py
+++ students/urls.py
@@ -2,9 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
+from students.views import StudentListView, StudentDetailView, StudentUpdateView, StudentDeleteView, StudentCreateView
 
 urlpatterns = [
-    path('', StudentsListView.as_view(), name='product_list'),
-
+    path('', StudentListView.as_view(), name='student_list'),
+    path('detail/<slug:slug>/', StudentDetailView.as_view(), name='student_detail'),
+    path('edit/<slug:slug>/', StudentUpdateView.as_view(), name='student_form'),
+    path('confirm_delete/<pk>/', StudentDeleteView.as_view(), name='student_confirm_delete'),
+    path('create/', StudentCreateView.as_view(), name='student_create'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
