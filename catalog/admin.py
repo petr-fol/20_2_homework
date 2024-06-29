a78edc19fb8ab270f651db360f13912f6096742d
Petr <folmerpetr21@gmail.com>
2024-06-29 17:09:46 +0500

    добавление категорий в админку

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, ProductVersion
+from catalog.models import Product, ProductVersion, ProductCategory
 
 admin.site.register(Product)
 
@@ -9,3 +9,9 @@ admin.site.register(Product)
 class VersionAdmin(admin.ModelAdmin):
     list_display = ('number_of_version', 'current_version', 'product')
     list_filter = ('number_of_version',)
+
+
+@admin.register(ProductCategory)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('name',)
