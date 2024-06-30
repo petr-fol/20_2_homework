b5200bf5198265b502fdf7e7da7596528b502c22
Petr <folmerpetr21@gmail.com>
2024-06-30 19:55:13 +0500

    + кастомная команда

---
+++ catalog/management/commands/new_dumpdata.py
@@ -0,0 +1,23 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Обновление данных в файлы дампа JSON'
+
+    def handle(self, *args, **kwargs):
+        fixtures = [
+            ('catalog.Product', 'catalog/fixtures/product.json'),
+            ('catalog.ProductVersion', 'catalog/fixtures/productversion.json'),
+            ('catalog.ProductCategory', 'catalog/fixtures/productcategory.json'),
+            ('blog.Message', 'blog/fixtures/message.json'),
+            ('students.Student', 'students/fixtures/student.json'),
+            ('users.User', 'users/fixtures/user.json'),
+        ]
+
+        for model, fixture in fixtures:
+                call_command('dumpdatautf8', model, indent=4, output=fixture)
+
+        call_command('dumpdatautf8', indent=4, output='data.json')
+
+        self.stdout.write(self.style.SUCCESS('Дампы успешно обновлены'))
