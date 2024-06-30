b8555c47a03829f7fc4bd08db64d53082f4c7ff5
Petr <folmerpetr21@gmail.com>
2024-06-30 19:56:06 +0500

    добавление стилей к формам

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from users.models import User
+from config.style import StyleFormMixin
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ['email', 'password1', 'password2']
 
 
-class UserProfileForm(UserChangeForm):
+class UserProfileForm(StyleFormMixin, UserChangeForm):
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name', 'phone', 'avatar']
